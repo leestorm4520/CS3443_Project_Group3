d8d4131808f474a20b332207e034fcabb8d48ff8
John Le <70299365+leestorm4520@users.noreply.github.com>
2021-04-14 11:49:35 -0500

    Update LogInEntranceController.java

--- WeShare/src/application/LogInEntranceController.java
+++ WeShare/src/application/LogInEntranceController.java
@@ -1,1 +1,22 @@
+package application;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class LogInEntraceController implements Initializable{
+  
+  public void OptionDisplay(ActionEvent event)
+  
+                            
+  public void initializable(URL arg0, ResourceBundle arg1){
+}
