c1603444b50a3aca63ef1f3121c4c5ca9112c3fd
John Le <70299365+leestorm4520@users.noreply.github.com>
2021-04-14 12:10:42 -0500

    Create ModelClass.java

---
+++ WeShare/src/application/ModelClass.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import java.util.regex.*;
+
+public class Model{
+  User userInfo=new User();
+  private static HashMap<String, userInfo> h=new HashMap<String, userInfo>();
+  private Properties properties=new Properties();
+ 
+  
+ Alert a=new Alert(AlertType.NONE);
+ public void user
+}
