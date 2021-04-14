689d584921f18358f74a511eab6abb6bd3a37410
John Le <70299365+leestorm4520@users.noreply.github.com>
2021-04-14 12:46:55 -0500

    Update Model.java

--- WeShare/src/application/Model.java
+++ WeShare/src/application/Model.java
@@ -12,16 +12,34 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import java.util.regex.*;
 
-public class Model{
+public class Model throws IOException{
   User userInfo=new User();
   private static HashMap<String, userInfo> h=new HashMap<String, userInfo>();
   private Properties properties=new Properties();
+  File file= new File("data.properties");
   Alert a=new Alert(AlertType.NONE);
   
-  public boolean userIDValidation(String id){
+  public boolean userFormatValidation(String id, String password){
     Pattern p=Pattern.compile("\w{8,15}"};
-    Matcher m=p.matcher(id);
-    if(!m.find()){
-      a.setAler
+    Matcher m1=p.matcher(id);
+    Matcher m2=p.matcher(password);
+    
+    if(!m1.find() || !m2.find()){
+      a.setAlertType(AlertType.ERROR);
+      a.setContentText("Either user id or password is not in the correct format");
+      a.show();
+  }
+                               
+  public boolean userExistenceValidation(String id, String password){
+   file.createNewFile();
+    FileInputStream reader=new FileInputStream(file);
+    properties.load(read);
+    reader.close();
+    for(String object: properties.stringPropertiesName()){
+      h.put(object, properties.get(object).toString());
+    
+    if(h.containsKey(id)){
+      
+    }
   }
 }
